ac33593734dab991b21b4b12992671363a9aa37a
lanaGithub04 <lanakuliah04@gmail.com>
2024-06-03 02:06:26 +0800

    Membuat Class MatematikaBeraksi

--- src/main/java/hitung/matematikaBeraksi.java
+++ src/main/java/hitung/matematikaBeraksi.java
@@ -1,6 +1,13 @@
-
 package hitung;
 
 public class matematikaBeraksi {
-    
+    public static void main(String [] args) {
+        //Membuat objek
+        Matematika lann = new Matematika(4,8);
+        
+      System.out.println("Hasil Penjumlahan: "+lann.setPenjumlahan());
+      System.out.println("Hasil Pengurangan: "+lann.setPengurangan());
+      System.out.println("Hasil Perkalian: "+lann.setPerkalian());
+      System.out.println("Hasil Pembagian: "+lann.setPembagian());
+    }
 }
